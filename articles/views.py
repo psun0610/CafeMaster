a0c8d717d21fcaab5a0ade6897b5eff085470ec9
tenedict <tenedict@gmail.com>
2022-11-01 17:22:05 +0900

    update:  article/models,urls,forms.py 방향성에 맞게 바꿈

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from .models import Comment, Cafe
+from .forms import CafeForm, CommentForm
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "articles/index.html")
+
+
+
+def create_cafe(request):
+    if request.method == "POST":
+        articleform = CafeForm(request.POST, request.FILES)
+        if articleform.is_valid():
+            article = articleform.save(commit=False)
+            article.save()
+        
+            return redirect("articles:index")
+    else:
+        articleform = CafeForm()
+    context = {"article_form": articleform}
+    return render(request, "articles/create_cafe.html", context)
+
+def create_comment(request):
+    if request.method == "POST":
+        commentForm = CommentForm(request.POST, request.FILES)
+        if commentForm.is_valid():
+            comment = commentForm.save(commit=False)
+            
+            comment.save()
+
+            return redirect("articles:index")
+    else:
+        commentForm = CommentForm()
+    context = {"article_form": commentForm}
+    return render(request, "articles/create_comment.html", context)
+
+

70118b2b2e0310d7ec10ecafa70f745210cf4c3f
JeongJinGan <rksaudwls123@naver.com>
2022-11-01 16:06:24 +0900

    Update: settings

--- CafeMaster/urls.py
+++ CafeMaster/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("articles/", include("articles.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+
+
+def index(request):
+    accounts = get_user_model().objects.order_by("-pk")
+    context = {"accounts": accounts}
+    return render(request, "accounts/index.html", context)
+
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()  # ModelForm의 save메소드의 리턴값은 해당 모델의 인스턴스다
+            auth_login(request, user)  # 로그인
+            return redirect("accounts:index")
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get("next") or "articles:index")
+    else:
+        form = AuthenticationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/login.html", context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect("accounts:login")

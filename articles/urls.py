ebecd1c7fe194b3d79fe5b6d20c6d0b4a6659f03
tenedict <tenedict@gmail.com>
2022-11-01 15:22:19 +0900

    debug

--- articles/urls.py
+++ articles/urls.py
@@ -5,9 +5,4 @@ app_name = 'articles'
 
 urlpatterns = [
   path('', views.index, name='index'), #기본페이지
-  path('create/', views.create, name='create'),
-  path('<int:pk>/', views.detail, name='detail'),
-  path('<int:pk>/update/', views.update, name='update'), #수정
-  path('<int:pk>/comments/', views.comment_create, name='comment_create'), #댓글
-  path('<int:pk>/like/', views.like, name='like'),
 ]

58556ecd843d406eb091c2561d2c0f6ae70ed6cc
DrugCoding <lhs940308@gmail.com>
2022-11-02 21:42:05 +0900

    Add: search.html,urls,views

--- articles/views.py
+++ articles/views.py
@@ -65,4 +65,5 @@ def create_comment(request, pk):
     context = {"commentform": commentForm}
     return render(request, "articles/create_comment.html", context)
 
-
+def search(request):
+    pass

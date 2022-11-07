5e587284b41253b2d1ca21336b4723a1bd80a39b
psun0610 <psy010610@naver.com>
2022-11-07 23:44:19 +0900

    헤로쿠 배포 12

--- articles/models.py
+++ articles/models.py
@@ -19,8 +19,8 @@ good = (
 
 
 class Cafe(models.Model):
-    name = models.CharField(max_length=20)
-    address = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=1000)
     map_url = models.TextField(default='')
     telephone = models.TextField()
     opening = models.TextField()
